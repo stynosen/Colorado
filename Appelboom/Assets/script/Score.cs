54be8e330a299e52f2d9e93f428a7e755fda8400
Stijn Doyen <stijndoyen@hotmail.com>
2014-11-18 01:02:54 +0100

    Finished!

--- Appelboom/Assets/script/Score.cs
+++ Appelboom/Assets/script/Score.cs
@@ -5,10 +5,12 @@ public class Score : MonoBehaviour
 {
     private bool m_Enjoyment = false;
     public int m_Score = 0;
+    public AudioClip m_Cheer;
     private float m_MaxHeight = -3.0f;
 
     private GameObject m_CherryPile;
     private float m_MinHeight;
+    private float m_CheerTimer = 0;
 	// Use this for initialization
 	void Start () 
     {
@@ -27,6 +29,21 @@ public class Score : MonoBehaviour
         {
             if (m_Enjoyment)
                 GetComponent<TextMesh>().text = m_Score.ToString();
+
+            if(m_CheerTimer > 0)
+            {
+                m_CheerTimer -= Time.deltaTime;
+                int time = (int)(m_CheerTimer * 100.0f);
+                if (time%10 == 0)
+                {
+                    GetComponent<TextMesh>().fontStyle = (GetComponent<TextMesh>().fontStyle == FontStyle.Bold) ? FontStyle.Normal : FontStyle.Bold;
+                }
+                if(m_CheerTimer < 0)
+                {
+                    m_CheerTimer = 0;
+                    GetComponent<TextMesh>().fontStyle = FontStyle.Bold;
+                }
+            }
         }
 	}
 
@@ -40,6 +57,12 @@ public class Score : MonoBehaviour
             Vector3 newPos = m_CherryPile.transform.localPosition;
             newPos.y = Mathf.Lerp(m_MinHeight, m_MaxHeight, ((float)Mathf.Min(m_Score - 1, CherryDropperManager.m_MaxScore) / CherryDropperManager.m_MaxScore));
             m_CherryPile.transform.localPosition = newPos;
+
+            if(m_Score % 10 == 0)
+            {
+                AudioSource.PlayClipAtPoint(m_Cheer, transform.position);
+                m_CheerTimer = 0.40f;
+            }
         }
     }
 
